b4659af6aea531534b1ab0af5c2c47ce2b322b1e
dian <sh.alqudree@gmail.com>
2022-02-17 16:33:07 +0800

    init

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,6 +2,7 @@ console.log("Hello World!");
 
 var prompt = require('prompt-sync')();
 var n = prompt("Enter name: ");
-
+var age = prompt("Enter your age: ");
 
 console.log("My Name is", n);
+console.log("I'm ", age);
