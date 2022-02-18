b6341b54b4f7111e3f729254243c476f93883c51
dian <sh.alqudree@gmail.com>
2022-02-19 01:04:52 +0800

    init

--- Fundamental of JavaScript/Object I.js
+++ Fundamental of JavaScript/Object I.js
@@ -1,14 +1,14 @@
-// Declare an Object
-
 //let name = 'darthemis';
 //let age = 21;
 
-let person = 
+// Declare an Object
+let person = //object
 {
     //key of the object
     name: 'darthemis',
     age: 21
 };
+
 console.log(person);
 
 //access the key of the object
